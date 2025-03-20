514dcef8fef5ee0d10210c26614d05b65da7fdee
Toni Gomez <tonito250@gmail.com>
2025-03-20 12:14:58 +0100

    Enlace del render

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include 'function.php'; // Incluye tu archivo function.php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Chatbot</title>
+    <style>
+        body {
+            background-color: black; /* Establece el fondo negro */
+            color: white; /* Cambia el color del texto a blanco para que se vea bien en el fondo negro */
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php agregar_chatbot(); // Llama a la función correctamente ?>
+</body>
+</html>
